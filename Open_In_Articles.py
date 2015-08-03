f16d43a973d110be6898d3df81029fe03db2cce3
Sam Weinberg <sweinberg@users.noreply.github.com>
2015-08-03 02:32:24 -0700

    Create Open_In_Articles.py

---
+++ Open_In_Articles.py
@@ -0,0 +1,19 @@
+#Bookmarklet
+#javascript:window.location='pythonista://Articles?action=run&argv='+encodeURIComponent(document.location.href);
+
+import clipboard
+import string
+import sys
+import webbrowser
+
+try:
+	article = sys.argv[1]
+except IndexError:
+	article = clipboard.get()
+
+article = article.replace('http://en.wikipedia.org/wiki/', 'x-articles://wikipedia.org/wiki/')
+article = article.replace('https://en.wikipedia.org/wiki/', 'x-articles://wikipedia.org/wiki/')
+article = article.replace('http://en.m.wikipedia.org/wiki/', 'x-articles://wikipedia.org/wiki/')
+article = article.replace('https://en.m.wikipedia.org/wiki/', 'x-articles://wikipedia.org/wiki/')
+
+webbrowser.open(article)
